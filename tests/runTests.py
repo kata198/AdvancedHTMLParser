4b949ef4aa1260f9b135303771c46a8cd77a05ee
Tim Savannah <kata198@gmail.com>
2015-10-15 22:45:22 -0400

    Update runTests version

--- tests/runTests.py
+++ tests/runTests.py
@@ -1,12 +1,29 @@
 #!/usr/bin/env python
+#
+#  Copyright (c) 2015 Tim Savannah under following terms:
+#   You may modify and redistribe this script with your project
+#
+# It will download the latest GoodTests.py and use it to execute the tests.
+#
+#  This should be placed in a directory, "tests", at the root of your project. It assumes that ../$MY_PACKAGE_MODULE is the path to your test module, and will create a symlink to it in order to run tests.
+#  The tests should be found in $MY_TEST_DIRECTORY in given "tests" folder.
 
-# vim: set ts=4 sw=4 expandtab :
 import os
+
 import glob
 import subprocess
 import sys
 
 GOODTESTS_URL = 'https://raw.githubusercontent.com/kata198/GoodTests/master/GoodTests.py'
+
+# This should be your module name, assumed to be in "../$MY_PACKAGE_MODULE". A symlink will be created in order to test that without messing with the global copy.
+#  If you set this to False, then a symlink will not be created and it will attempt to use the module as found in PYTHONPATH, or abort if it cannot be found.
+MY_PACKAGE_MODULE = 'AdvancedHTMLParser'
+# MY_PACKAGE_MODULE = False
+
+# This is the test directory that should contain all your tests. This should be a directory in your "tests" folder
+# TODO: modify this to  support a relative apth anywhere
+MY_TEST_DIRECTORY = 'AdvancedHTMLParserTests'
 
 def findGoodTests():
     pathSplit  = os.environ['PATH'].split(':')
@@ -78,12 +95,15 @@ def download_goodTests():
 
 if __name__ == '__main__':
 
+    thisDir = os.path.dirname(__file__)
+    if not thisDir:
+        thisDir = os.getcwd()
+    elif not thisDir.startswith('/'):
+        thisDir = os.getcwd() + '/' + thisDir
+
+    os.chdir(thisDir)
+
     didDownload = False
-    try:
-        import AdvancedHTMLParser
-    except ImportError:
-        sys.stderr.write('Could not import AdvancedHTMLParser. Either install it, or make a symlink in this directory to ../AdvancedHTMLParser\n')
-        sys.exit(1)
     
     goodTestsInfo = findGoodTests()
     if goodTestsInfo['success'] is False:
@@ -93,8 +113,25 @@ if __name__ == '__main__':
             sys.stderr.write('Could not download or find GoodTests.py. Try  to download it yourself using "pip install GoodTests",  or wget %s\n' %( GOODTESTS_URL,))
             sys.exit(1)
 
+    if MY_PACKAGE_MODULE and not os.path.exists(MY_PACKAGE_MODULE):
+        if not os.path.exists('../' + MY_PACKAGE_MODULE):
+            sys.stderr.write('Expected to find ../%s given MY_PACKAGE_MODULE, but not present. Have you unpacked all the source?\n' %(MY_PACKAGE_MODULE,))
+            sys.exit(1)
+
+        try:
+            os.symlink(MY_PACKAGE_MODULE, '../'  + MY_PACKAGE_MODULE)
+        except Exception as e:
+            sys.stderr.write('Unable to create a symlink to ../%s: %s\n' %(MY_PACKAGE_MODULE, str(e)))
+            sys.exit(1)
+
+    try:
+        __import__(MY_PACKAGE_MODULE)
+    except ImportError:
+        sys.stderr.write('Could not import %s. Either install it or otherwise add to PYTHONPATH\n' %(MY_PACKAGE_MODULE,))
+        sys.exit(1)
+
     sys.stdout.write('Starting test..\n')
     sys.stdout.flush()
     sys.stderr.flush()
-    pipe = subprocess.Popen(goodTestsInfo['path'] + ['AdvancedHTMLParserTests'], shell=False)
+    pipe = subprocess.Popen(goodTestsInfo['path'] + [MY_TEST_DIRECTORY], shell=False)
     pipe.wait()
