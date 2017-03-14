654ffce0af35d5d2d656d4d99252b670b1f00683
Tim Savannah <kata198@gmail.com>
2017-03-14 16:06:13 -0500

    Add comments and tests for AdvancedTag.getAttributesList and AdvancedTag.getAttributesDict

--- tests/AdvancedHTMLParserTests/test_Attributes.py
+++ tests/AdvancedHTMLParserTests/test_Attributes.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env GoodTests.py
 '''
-    Test various attribute getting/setting
+    Test various attribute related things
 '''
 
 import sys
@@ -8,8 +8,8 @@ import subprocess
 
 from AdvancedHTMLParser.Tags import AdvancedTag
 from AdvancedHTMLParser.Parser import AdvancedHTMLParser
+from AdvancedHTMLParser.SpecialAttributes import StyleAttribute
 
-# TODO: add tests for getAttributesList and getAttributesDict
 
 class TestAttributes(object):
     '''
@@ -52,6 +52,117 @@ class TestAttributes(object):
 
         assert foundCheese1
         assert foundCheese2
+
+
+    def test_getAttributesList(self):
+        parser = AdvancedHTMLParser()
+
+        parser.parseStr('<div id="hello" style="display: none; width: 500px; padding-left: 15px;" class="One Two" data="Yes">Hello</div>')
+
+        helloEm = parser.getElementById('hello')
+
+        assert helloEm.getAttribute('id', '') == 'hello' , 'Got unxpected element'
+
+        attributesList = helloEm.getAttributesList()
+
+        foundId = False
+        foundStyle = False
+        foundClass = False
+        foundData = False
+
+        for attrName, attrValue in attributesList:
+            if attrName == 'id':
+                assert attrValue == 'hello' , 'Attribute "id" did not have expected value "hello", got "%s"' %(attrValue,)
+
+                foundId = True
+            elif attrName == 'style':
+                
+                style = StyleAttribute(attrValue)
+                assert style.display == 'none', 'Got unexpected value for display in style copy. Expected "none", got "%s"' %(style.display,)
+                assert style.width == '500px' , 'Got unexpected value for width in style copy. Expected "500px", got "%s"' %(style.width,)
+                assert style.paddingLeft == '15px', 'Got unexpected value for padding-left. Expected "15px", got "%s"' %(style.paddingLeft, )
+                
+                foundStyle = True
+            elif attrName == 'class':
+
+                assert attrValue == 'One Two', 'Expected class name to equal "One Two", got: %s' %(attrValue, )
+
+                foundClass = True
+            elif attrName == 'data':
+
+                assert attrValue == 'Yes', 'Expected attribute "data" to have the value "Yes", got: %s' %(attrValue, )
+
+                foundData = True
+
+            else:
+                raise AssertionError('Got unexpected attribute in copy: (%s, %s)' %(attrName, attrValue))
+
+
+        assert foundId is True , 'Did not find id element in attribute list'
+        assert foundStyle is True , 'Did not find style element in attribute list'
+        assert foundClass is True , 'Did not find class element in attribute list'
+        assert foundData is True , 'Did not find data element in attribute list'
+
+        # Test that we have a COPY, not the originals
+
+        for item in attributesList:
+            if item[0] == 'style':
+                # Just incase in the future we want to include a StyleAttribute instead of the str
+                if not isinstance(item[1], StyleAttribute):
+                    style = StyleAttribute(item[1])
+                else:
+                    style = item[1]
+                style.paddingTop = '10px'
+
+
+        
+        # These should not be modified in the original element
+        assert 'padding-top' not in str(helloEm.style)
+#        parser.parseStr('<div id="hello" style="display: none; width: 500px; padding-left: 15px;" class="One Two" data="Yes">Hello</div>')
+
+    def test_getAttributesDict(self):
+        parser = AdvancedHTMLParser()
+
+        parser.parseStr('<div id="hello" style="display: none; width: 500px; padding-left: 15px;" class="One Two" data="Yes">Hello</div>')
+
+        helloEm = parser.getElementById('hello')
+
+        assert helloEm.getAttribute('id', '') == 'hello' , 'Got unxpected element'
+
+        attributesDict = helloEm.getAttributesDict()
+
+        assert 'id' in attributesDict , 'Did not find "id" in the attributes dict copy'
+        assert 'style' in attributesDict , 'Did not find "style" in the attributes dict copy'
+        assert 'class' in attributesDict , 'Did not find "class" in the attributes dict copy'
+        assert 'data' in attributesDict , 'Did not find "data" in the attributes dict copy'
+
+        assert len(attributesDict.keys()) == 4 , 'Got unexpected keys in attributesDict. Only expected "id" "style" "class" and "data", got: "%s"' %(repr(attributesDict),)
+
+        assert attributesDict['id'] == 'hello' , 'Attribute "id" did not have expected value "hello", got "%s"' %(attributesDict['id'],)
+
+        style = StyleAttribute(attributesDict['style'])
+        assert style.display == 'none', 'Got unexpected value for display in style copy. Expected "none", got "%s"' %(style.display,)
+        assert style.width == '500px' , 'Got unexpected value for width in style copy. Expected "500px", got "%s"' %(style.width,)
+        assert style.paddingLeft == '15px', 'Got unexpected value for padding-left. Expected "15px", got "%s"' %(style.paddingLeft, )
+
+        assert attributesDict['class'] == 'One Two', 'Got unexpected value for "class" in dict copy. Expected "One Two", Got: "%s"' %(attributesDict['class'], )
+
+        assert attributesDict['data'] == 'Yes' , 'Got unexpected value for "data" in dict copy, Expected "Yes", Got: "%s"' %(attributesDict['data'], )
+
+        # Assert we aren't modifying the original element
+        style.paddingTop = '13em'
+
+        assert helloEm.style.paddingTop != '13em' , 'Expected getAttributesDict to return copies, but modified original element on "style"'
+
+        attributesDict['class'] += ' Three'
+
+        assert 'Three' not in helloEm.getAttribute('class') , 'Expected getAttributesDict to return copies, but modified original element on "class"'
+
+        attributesDict['id'] = 'zzz'
+
+        assert helloEm.getAttribute('id') != 'zzz' , 'Expected getAttributesDict to return copies, but modified original element on "id"'
+
+
 
     def test_setAttributes(self):
         tag = AdvancedTag('div')
