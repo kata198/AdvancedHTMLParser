0045b3450ce5c97a75c82b08de49482b1b321125
Tim Savannah <kata198@gmail.com>
2017-04-28 17:45:14 -0400

    7.0.2

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ if __name__ == '__main__':
         long_description = summary
 
     setup(name='AdvancedHTMLParser',
-            version='7.0.1',
+            version='7.0.2',
             packages=['AdvancedHTMLParser'],
             scripts=['formatHTML'],
             author='Tim Savannah',
