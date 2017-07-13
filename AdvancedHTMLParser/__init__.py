892e913fe2bd6556028186fcdbb5aea47d0ffaf1
Tim Savannah <kata198@gmail.com>
2017-07-13 14:20:12 -0400

    7.2.1

--- AdvancedHTMLParser/__init__.py
+++ AdvancedHTMLParser/__init__.py
@@ -10,9 +10,9 @@ from .Validator import ValidatingAdvancedHTMLParser
 from .exceptions import InvalidCloseException, MissedCloseException, HTMLValidationException, MultipleRootNodeException
 from .SpecialAttributes import StyleAttribute
 
-__version__ = '7.2.0'
-__version_tuple__ = ('7', '2', '0')
-__int_version_tuple__ = (7, 2, 0)
+__version__ = '7.2.1'
+__version_tuple__ = ('7', '2', '1')
+__int_version_tuple__ = (7, 2, 1)
 
 __all__ = ( 'AdvancedHTMLParser', 'IndexedAdvancedHTMLParser', 'AdvancedHTMLFormatter', 'AdvancedTag', 'TagCollection',
     'ValidatingAdvancedHTMLParser', 'MissedCloseException', 'InvalidCloseException', 'HTMLValidationException', 'MultipleRootNodeException',
