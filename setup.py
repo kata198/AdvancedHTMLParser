dfb60e4a5f90a3f4675f519c4207542c22237f44
Tim Savannah <kata198@gmail.com>
2017-11-19 15:47:57 -0500

    update version in setup.py

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ if __name__ == '__main__':
         long_description = summary
 
     setup(name='AdvancedHTMLParser',
-            version='7.2.3.2',
+            version='7.3.0',
             packages=['AdvancedHTMLParser'],
             scripts=['formatHTML'],
             author='Tim Savannah',
