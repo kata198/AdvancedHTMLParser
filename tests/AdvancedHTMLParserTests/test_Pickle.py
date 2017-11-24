fcea3c306fa42657dccc9e9b74b67e5968e64076
Tim Savannah <kata198@gmail.com>
2017-11-24 17:15:48 -0500

    Fix pickling with basic mostly-working impl

---
+++ tests/AdvancedHTMLParserTests/test_Pickle.py
@@ -0,0 +1,171 @@
+#!/usr/bin/env GoodTests.py
+'''
+    Test pickling
+'''
+
+import pickle
+import subprocess
+import sys
+
+import AdvancedHTMLParser
+
+AdvancedTag = AdvancedHTMLParser.AdvancedTag
+
+class TestPickle(object):
+    '''
+        A general test class. Basically example.py converted a bit.
+
+    '''
+
+    def test_pickleTagGeneral(self):
+        '''
+            pickleTagGeneral - Test simple pickling a lone AdvancedTag
+        '''
+
+        # Test just a general tag first
+        tag = AdvancedHTMLParser.AdvancedTag('div')
+        tag.id = 'myDiv'
+        tag.className = 'one two'
+        
+        pickleStr = pickle.dumps(tag)
+
+        assert pickleStr , "Failed to get a pickle dump from AdvancedTag"
+
+        loadedTag = pickle.loads(pickleStr)
+
+        assert issubclass(loadedTag.__class__, AdvancedHTMLParser.AdvancedTag) , 'Failed to get an AdvancedTag object loading from pickled string'
+
+        assert loadedTag.id == 'myDiv' , 'Pickle failed to retain id of tag'
+        assert loadedTag.className == 'one two' , 'Pickle failed to retain className'
+        assert loadedTag.tagName == 'div' , 'Pickle failed to retain tagName'
+        assert loadedTag.uid == tag.uid , "Failed to retain uid after restore from pickle"
+
+
+    def test_pickleTagComplex(self):
+        '''
+            test_pickleTagComplex - Test more complex variations on a tag
+        '''
+        mainTag = AdvancedTag('div')
+        mainTag.id = 'mainDiv'
+        mainTag.setAttribute('x-cheese', 'cheddar')
+        
+        subspan1 = AdvancedTag('span')
+        subspan1.setAttribute('id', 'subspan1')
+        subspan1.name = 'subspans'
+        subspan1.style = 'display: block; float: left; clear: both;'
+
+        subspan1.appendText("My Text")
+
+        mainTag.appendChild(subspan1)
+
+        subspan2 = AdvancedTag('span')
+        subspan2.setAttribute('id', 'subspan2')
+        subspan2.name = 'subspans'
+        subspan2.className = "heavy-text quoteblock"
+
+        img = AdvancedTag('img')
+        img.src = '/images/cheese.png'
+        img.style.width = '80px'
+        img.style.height = '60px'
+
+        subspan2.appendChild(img)
+
+        mainTag.appendChild(subspan2)
+
+
+        pickleStr = pickle.dumps(mainTag)
+        
+        assert pickleStr , "Was unable to get a pickle string from complex AdvancedTag"
+
+        loadedMainTag = pickle.loads(pickleStr)
+
+        assert issubclass(loadedMainTag.__class__, AdvancedTag) , "Loaded pickle string but did not get AdvancedTag back. Got: " + type(loadedMainTag).__name__
+
+        assert loadedMainTag.id == "mainDiv" , "Loaded pickle did not retain id on mainDiv"
+        assert loadedMainTag.getAttribute("x-cheese") == "cheddar" , "Loaded pickle did not retain 'x-cheese' attribute."
+        
+        assert len(loadedMainTag.children) == 2 , "Loaded pickle did not retain 2 children. children property is: " + repr(loadedMainTag.children)
+
+        loadedChild1 = loadedMainTag.children[0]
+        assert loadedChild1.tagName == 'span' , "Expected first child on loaded pickle to have tagName='span'. Got: " + repr(loadedChild1.tagName)
+        assert loadedChild1.id == 'subspan1' , "Expected first child on loaded pickle to have id='subspan1'. Got: " + repr(loadedChild1.id)
+        assert loadedChild1.name == 'subspans' , 'Expected first child on loaded pickle to have name="subspans". Got: ' + repr(loadedChild1.name)
+
+        for styleName, styleValue in [ ('display', 'block'), ('float', 'left'), ('clear', 'both') ]:
+            loadedChild1StyleVal = getattr(loadedChild1.style, styleName)
+            assert loadedChild1StyleVal == styleValue , 'Wrong value for style property "%s" on first child. Expected "%s" but got "%s"' %(styleName, styleValue, loadedChild1StyleVal)
+        
+        loadedChild1StartTag = loadedChild1.getStartTag()
+        assert "display: block" in loadedChild1StartTag , 'style.display missing in HTML representation after pickle load. Got: ' + repr(loadedChild1StartTag)
+        
+        assert "<span" in loadedChild1StartTag, "Missing correct tag name in HTML representation after pickle load. Got: " + repr(loadedChild1StartTag)
+
+        assert "My Text" in loadedChild1.text , "Expected 'My Text' to appear in .text attribute. Got: " + repr(loadedChild1.text)
+
+        assert "My Text" in str(loadedChild1) , "Expected 'My Text' to appear in HTML reprsentation. Got: " + repr(str(loadedChild1))
+
+        loadedChild2 = loadedMainTag.children[1]
+
+        assert loadedChild2.tagName == 'span' , 'Expected tagName to be "span", but got: ' + repr(loadedChild2.tagName)
+        assert loadedChild2.id == 'subspan2' , 'Expected id="subspan2" but got: ' + repr(loadedChild2.id)
+        assert loadedChild2.getAttribute('name') == 'subspans' , "Expected name='subspans' but got: " + repr(loadedChild2.getAttribute('name'))
+
+        assert 'heavy-text' in loadedChild2.classList , "Expected 'heavy-text' to be in classList, but got: " + repr(loadedChild2.classList)
+        assert loadedChild2.className == 'heavy-text quoteblock' , 'Expected className to be "heavy-text quoteblock" but got: ' + repr(loadedChild2.className)
+
+        assert len(loadedChild2.children) == 1 , 'Expected 1 child node for subspan2. Children are: ' + repr(loadedChild2.children)
+
+
+        loadedImgTag = loadedChild2.children[0]
+
+        assert loadedImgTag.tagName == 'img' , 'Expected tagName="img" but got: ' + repr(loadedImgTag.tagName)
+        assert loadedImgTag.src == '/images/cheese.png' , 'Got unexpected "src" attribute. Got: ' + repr(loadedImgTag.src)
+        
+        loadedImgTagStyle = loadedImgTag.style
+        assert loadedImgTagStyle.width == '80px' , 'Expected width: 80px but got: ' + repr(loadedImgTagStyle.width)
+        assert loadedImgTagStyle.height == '60px' , 'Expected height: 60px but got: ' + repr(loadedImgTagStyle.height)
+
+        assert 'height: 60px' in str(loadedImgTagStyle) and 'width: 80px' in str(loadedImgTagStyle) , 'Expected style to contain both "width: 80px" and "height: 60px" but it was: ' + repr(str(loadedImgTagStyle))
+
+        assert len(loadedImgTag.children) == 0 , 'Expected image tag to have no children'
+        assert loadedImgTag.uid == img.uid , 'Did not retain uid on img. Expected "%s" but got "%s"' %(str(img.uid), str(loadedImgTag.uid))
+
+
+    def test_pickleParser(self):
+        '''
+            test_pickleParser - Test pickling the parser
+        '''
+        document = AdvancedHTMLParser.AdvancedHTMLParser()
+
+        document.parseStr('''
+<html>
+    <head>
+        <title>Hello World Page</title>
+    </head>
+    <body bgcolor="black" style="background-color: black; color: white">
+        <div id="main">
+            <h2>This is a test page</h2>
+            <br />
+            <br />
+            <div id="subDiv">
+                <span id="text1">This</span> is a sub div
+                <div class="some-div" >
+                    Embedded again
+                </div>
+                <div class="some-div cheese" style="font-weight: bold">
+                    Some bold embedded test
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
+''')
+        pickleStr = pickle.dumps(document)
+
+        assert pickleStr , "Failed to get a pickle str for an AdvancedHTMLParser.AdvancedHTMLParser assembled with parseStr."
+
+
+
+
+if __name__ == '__main__':
+    sys.exit(subprocess.Popen('GoodTests.py -n1 "%s" %s' %(sys.argv[0], ' '.join(['"%s"' %(arg.replace('"', '\\"'), ) for arg in sys.argv[1:]]) ), shell=True).wait())
