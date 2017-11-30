091fb7fcc8b7d063587a150d2d1e9de8099a0c35
Tim Savannah <kata198@gmail.com>
2017-11-30 16:20:04 -0500

    Re-release 7.4.0 as 8.0.0 to signify that it is a major update

--- AdvancedHTMLParser/__init__.py
+++ AdvancedHTMLParser/__init__.py
@@ -10,9 +10,9 @@ from .Validator import ValidatingAdvancedHTMLParser
 from .exceptions import InvalidCloseException, MissedCloseException, HTMLValidationException, MultipleRootNodeException
 from .SpecialAttributes import StyleAttribute
 
-__version__ = '7.4.0'
-__version_tuple__ = ('7', '4', '0')
-__int_version_tuple__ = (7, 4, 0)
+__version__ = '8.0.0'
+__version_tuple__ = ('8', '0', '0')
+__int_version_tuple__ = (8, 0, 0)
 
 __all__ = ( 'AdvancedHTMLParser', 'IndexedAdvancedHTMLParser', 'AdvancedHTMLFormatter', 'AdvancedTag', 'TagCollection',
     'ValidatingAdvancedHTMLParser', 'MissedCloseException', 'InvalidCloseException', 'HTMLValidationException', 'MultipleRootNodeException',
--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ if __name__ == '__main__':
         long_description = summary
 
     setup(name='AdvancedHTMLParser',
-            version='7.4.0',
+            version='8.0.0',
             packages=['AdvancedHTMLParser'],
             scripts=['formatHTML'],
             author='Tim Savannah',
