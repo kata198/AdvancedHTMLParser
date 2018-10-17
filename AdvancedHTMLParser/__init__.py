01fa4fa5414cc1df86189f8b531faed702073c8d
Tim Savannah <kata198@gmail.com>
2018-10-17 00:08:41 -0400

    8.1.3

--- AdvancedHTMLParser/__init__.py
+++ AdvancedHTMLParser/__init__.py
@@ -14,9 +14,9 @@ from .Validator import ValidatingAdvancedHTMLParser
 from .exceptions import InvalidCloseException, MissedCloseException, HTMLValidationException, MultipleRootNodeException
 from .SpecialAttributes import StyleAttribute
 
-__version__ = '8.1.2'
-__version_tuple__ = ('8', '1', '2')
-__int_version_tuple__ = (8, 1, 2)
+__version__ = '8.1.3'
+__version_tuple__ = ('8', '1', '3')
+__int_version_tuple__ = (8, 1, 3)
 
 __all__ = ( 'AdvancedHTMLParser', 'IndexedAdvancedHTMLParser', 'AdvancedHTMLFormatter', 'AdvancedTag', 'TagCollection',
     'ValidatingAdvancedHTMLParser', 'MissedCloseException', 'InvalidCloseException', 'HTMLValidationException', 'MultipleRootNodeException',
--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ if __name__ == '__main__':
         long_description = summary
 
     setup(name='AdvancedHTMLParser',
-            version='8.1.2',
+            version='8.1.3',
             packages=['AdvancedHTMLParser'],
             scripts=['formatHTML'],
             author='Tim Savannah',
