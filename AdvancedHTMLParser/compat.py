e2195b7fb7d282862a4a07819d3a1dbe451cf0de
Tim Savannah <kata198@gmail.com>
2019-11-25 22:00:18 -0500

    Add py2/py3 compat methods for ensuring an encoded string

--- AdvancedHTMLParser/compat.py
+++ AdvancedHTMLParser/compat.py
@@ -10,10 +10,10 @@
 
 import sys
 
-__all__ = ('STRING_TYPES', 'RAW_STRING_TYPE', 'ALL_STRING_TYPES')
+__all__ = ('STRING_TYPES', 'RAW_STRING_TYPE', 'ALL_STRING_TYPES', 'ensureStringEncoded')
 
 if sys.version_info.major < 3:
-    
+
     # STRING_TYPES - Types that represent strings ("printable")
     STRING_TYPES = (str, unicode)
 
@@ -23,8 +23,11 @@ if sys.version_info.major < 3:
     # ALL_STRING_TYPES - All string-like types, encoded or otherwise
     ALL_STRING_TYPES = (str, unicode)
 
+    # DECODED_STR_TYPE - String type that has been decoded
+    DECODED_STR_TYPE = unicode
+
 else:
-    
+
     # STRING_TYPES - Types that represent strings ("printable")
     STRING_TYPES = (str, )
 
@@ -33,5 +36,30 @@ else:
 
     # ALL_STRING_TYPES - All string-like types, encoded or otherwise
     ALL_STRING_TYPES = (str, bytes)
+
+    # DECODED_STR_TYPE - String type that has been decoded
+    DECODED_STR_TYPE = str
+
+
+def ensureStringEncoded(theString, encoding='utf-8'):
+    '''
+        ensureStringEncoded - Ensure we have the encoded type for a given string
+
+
+            @param theString <str/unicode/bytes> - A string-like object
+
+            @param encoding <str> Default 'utf-8' - The encoding to use
+
+                NOTE: If this string is already encoded, we do NOT ensure it is encoded in this type,
+                  this type is only used when we have a decoded string, in order to encode it.
+
+
+            @return (python3)<bytes> / (python2)<str> - A string encoded in utf-8
+    '''
+
+    if issubclass( theString.__class__, DECODED_STR_TYPE ):
+        return theString.encode('utf-8')
+
+    return theString
 
 # vim: set ts=4 sw=4 st=4 expandtab :
